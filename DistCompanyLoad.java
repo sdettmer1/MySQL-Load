8604d9291d60c9c3c32a25bce1d53198040b3657
sdettmer1 <97929213+sdettmer1@users.noreply.github.com>
2022-10-06 22:00:51 -0400

    add javadoc comments to 3 more classes

--- DistCompanyLoad.java
+++ DistCompanyLoad.java
@@ -2,7 +2,14 @@ import java.util.*;
 import java.sql.*;
 import java.io.*;
 
-
+/**
+* This class adds data from a ~ delimited text file to the film_company
+* table (type = 2) in the movies database
+*
+* @author Shane Dettmer
+* @version 1.1
+* @since 2022-08-29
+*/
 public class DistCompanyLoad{
 
    private Connection connection;
@@ -44,7 +51,13 @@ public class DistCompanyLoad{
       "C:\\Users\\Shane Dettmer\\OneDrive\\Documents\\Movies\\Backups\\";
 
 
-
+   /**
+   * Accepts the input date of the text file
+   * from the command line.  It will establish the MySQL connection
+   * create the needed files and initiate the loading process
+   *
+   * @param inputDate This is the date of the text file entered on command line
+   */
    public DistCompanyLoad( String inputDate )
    {
 
@@ -134,6 +147,12 @@ public class DistCompanyLoad{
 
    }
 
+   /**
+   * This method loops through the text file, extracting the 
+   * data from each record and then INSERTs a new record into the 
+   * table.
+   * 
+   */
    public void distCompanyLoad()
    {
 
@@ -195,7 +214,10 @@ public class DistCompanyLoad{
    }
 
 
-
+   /**
+   * Reads next record from input file
+   *
+   */
    private void readdistCompanyRecord() {
    
 
@@ -212,7 +234,11 @@ public class DistCompanyLoad{
       }
    }
 
-
+   /**
+   * Inserts a record into the database table
+   *
+   *@param query SQL statement that will be executed
+   */
    public void insertTable( String query )
    {
       
@@ -245,7 +271,13 @@ public class DistCompanyLoad{
 
    }
 
-
+   /**
+   * Searches for an apostrophe in a given String and then
+   * adds a new apostrophe next to it to prevent SQL errors
+   * 
+   * @param name the String to be edited
+   * @return String Returms the edited version of the input String
+   */
    public static String editForApostrophe( String name )
    {
 
@@ -265,6 +297,11 @@ public class DistCompanyLoad{
       return editedInput;
    }
 
+   /**
+   * Writes control totals to the output file and
+   * closes all resources and exits the program
+   *
+   */
    public void exitProgram()
    {
 
@@ -280,11 +317,26 @@ public class DistCompanyLoad{
 
        out.close();
 
+      try {
+         connection.close();
+      } catch(SQLException e) {
+         System.out.println( "Error occurred when closing SQL connection:" );
+         System.out.println( "Error Code " + e.getErrorCode() );
+         System.out.println( "SQL State: " + e.getSQLState() );
+         e.printStackTrace();
+         System.exit(-1);
+      }
+
        System.exit( 0 );
 
    }
    
-
+   /**
+   * Creates an instance of the class and runs the application
+   * One argument is necessary on the command line
+   *
+   * @param args The date portion of the file name in yyyy-mm-dd format
+   */
    public static void main( String args[] )
    {
       String date = args[ 0 ];
