72a7fdcade3d54057a4d5b08c3e7ee81c0e404db
sdettmer1 <97929213+sdettmer1@users.noreply.github.com>
2022-10-13 22:10:12 -0400

    add javadoc comments to four classes

--- IDTranslate.java
+++ IDTranslate.java
@@ -3,21 +3,46 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Connection;
 
-
+/**
+* This class translates the old movie id to 
+* the new movie id number via the id_translation table
+*
+* @author Shane Dettmer
+* @version 1.1
+* @since 2022-08-09
+*/
 public class IDTranslate {
 
    private int newID;
 
+   /**
+   * This is a getter to return the new movieID value
+   *
+   * @return int The new movieID value
+   */
    public int getNewID() {
 
       return newID;
    }
 
+   /**
+   * This is a setter to for the new movieID value
+   *
+   * @param newID The new movieID value
+   */
    public void setNewID(int newID) {
       this.newID = newID;   
       
    }
 
+   /**
+   * Reads the id_translation table to convert the old movieID
+   * value to the new movieID value
+   * 
+   * @param connection The RDBMS connection
+   * @param ID The old movie ID value to be converted
+   * @return int The new movie ID value
+   */
    public int getNewMovieID( Connection connection, int ID ) {
 
       String query = "SELECT new_id FROM movies.id_translation WHERE old_id=" + ID;
